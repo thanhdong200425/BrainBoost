7b56af5ec782fec468e4375d91f36929ad302405
DoanTrinh30 <trinhdoan.300304@gmail.com>
2025-03-08 21:08:27 +0700

    add login UI

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+    return <Text>This is home page</Text>;
+}
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace('/login');
+    }, [])
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="login" options={{ initial: true }} />
-            
+            <Stack.Screen name="login" />
+            {/* index => khong dung () => /tabs/index */}
+            {/* index => dung ()  => /index */}
         </Stack>
     );
 }
