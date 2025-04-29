63695ee9976f4808fcb967848cca28d7dec9553f
ThanhDong <dongnt.22ite@vku.udn.vn>
2025-04-29 08:59:45 +0700

    feat: Refactor login and index components to improve authentication flow and error handling, and add access token fetching

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,36 @@
-import { Redirect } from 'expo-router';
+import { useQuery } from '@tanstack/react-query'
+import { Redirect } from 'expo-router'
+import { fetchAccessToken } from '../services/authService'
+import { ActivityIndicator } from 'react-native'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setCredentials } from '../redux/slices/authSlice'
 
 export default function Index() {
-  return <Redirect href="/onboarding" />;
+    const dispatch = useDispatch()
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['auth'],
+        queryFn: fetchAccessToken,
+        staleTime: 1000 * 60 * 60 * 24,
+    })
+
+    useEffect(() => {
+        if (data?.token) {
+            dispatch(setCredentials({ accessToken: data.token }))
+        }
+    }, [data, dispatch])
+
+    if (isLoading)
+        return (
+            <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                style={{ flex: 1, justifyContent: 'center' }}
+            />
+        )
+
+    if (accessToken) return <Redirect href="/bottom/home" />
+
+    return <Redirect href="/onboarding" />
 }
--- services/authService.js
+++ services/authService.js
@@ -1,4 +1,5 @@
-import serverApi from '../helpers/axios';
+import serverApi from '../helpers/axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const signUp = async ({ email, password, confirmPassword }) => {
     try {
@@ -6,33 +7,47 @@ export const signUp = async ({ email, password, confirmPassword }) => {
             email,
             password,
             confirmPassword,
-        });
+        })
 
-        return response.data;
+        return response.data
     } catch (error) {
         if (error.response)
             throw new Error(
-                error.response.data?.message || 'Server error occurred during sign up.'
-            );
-        else if (error.request) throw new Error('Network Error: Unable to connect to server.');
-        else throw new Error('An unexpected error occurred during sign up.');
+                error.response.data?.message ||
+                    'Server error occurred during sign up.',
+            )
+        else if (error.request)
+            throw new Error('Network Error: Unable to connect to server.')
+        else throw new Error('An unexpected error occurred during sign up.')
     }
-};
+}
 
 export const signIn = async ({ email, password }) => {
     try {
         const response = await serverApi.post('/auth/sign-in', {
             email,
             password,
-        });
+        })
 
-        return response.data;
+        return response.data
     } catch (error) {
         if (error.response)
             throw new Error(
-                error.response.data?.message || 'Server error occurred during sign in.'
-            );
-        else if (error.request) throw new Error('Network Error: Unable to connect to server.');
-        else throw new Error('An unexpected error occurred during sign in.');
+                error.response.data?.message ||
+                    'Server error occurred during sign in.',
+            )
+        else if (error.request)
+            throw new Error('Network Error: Unable to connect to server.')
+        else throw new Error('An unexpected error occurred during sign in.')
     }
-};
+}
+
+export const fetchAccessToken = async () => {
+    try {
+        const token = await AsyncStorage.getItem('token')
+        return { token }
+    } catch (error) {
+        console.log('Error fetching token:', error)
+        return { token: null }
+    }
+}
